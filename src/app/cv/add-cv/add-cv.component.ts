5a98aebac21596dceaffba0a6fd4ed0d7a5fe2b6
aymensellaouti <aymen.sellaouti@gmail.com>
2025-10-10 07:57:06 +0100

    fix bug

--- src/app/cv/add-cv/add-cv.component.ts
+++ src/app/cv/add-cv/add-cv.component.ts
@@ -1,23 +1,24 @@
-import { Component } from "@angular/core";
-import { AbstractControl, FormBuilder, Validators, FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { CvService } from "../services/cv.service";
-import { Router } from "@angular/router";
-import { ToastrService } from "ngx-toastr";
-import { APP_ROUTES } from "src/config/routes.config";
-import { Cv } from "../model/cv";
-import { NgIf, JsonPipe } from "@angular/common";
+import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CvService } from '../services/cv.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { APP_ROUTES } from 'src/config/routes.config';
+import { Cv } from '../model/cv';
+import { NgIf, JsonPipe } from '@angular/common';
 
 @Component({
-    selector: "app-add-cv",
-    templateUrl: "./add-cv.component.html",
-    styleUrls: ["./add-cv.component.css"],
-    standalone: true,
-    imports: [
-        FormsModule,
-        ReactiveFormsModule,
-        NgIf,
-        JsonPipe,
-    ],
+  selector: 'app-add-cv',
+  templateUrl: './add-cv.component.html',
+  styleUrls: ['./add-cv.component.css'],
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule, NgIf, JsonPipe],
 })
 export class AddCvComponent {
   constructor(
@@ -27,57 +28,43 @@ export class AddCvComponent {
     private formBuilder: FormBuilder
   ) {}
 
-  form = this.formBuilder.group(
-    {
-      name: ["", Validators.required],
-      firstname: ["", Validators.required],
-      path: [""],
-      job: ["", Validators.required],
-      cin: [
-        "",
-        {
-          validators: [Validators.required, Validators.pattern("[0-9]{8}")],
-        },
-      ],
-      age: [
-        0,
-        {
-          validators: [Validators.required],
-        },
-      ],
-    },
-  );
+  form = this.formBuilder.group({
+    name: ['', Validators.required],
+    firstname: ['', Validators.required],
+    path: [''],
+    job: ['', Validators.required],
+    cin: [
+      '',
+      {
+        validators: [Validators.required, Validators.pattern('[0-9]{8}')],
+      },
+    ],
+    age: [
+      0,
+      {
+        validators: [Validators.required],
+      },
+    ],
+  });
 
-  addCv() {
-    this.cvService.addCv(this.form.value as Cv).subscribe({
-      next: (cv) => {
-        this.router.navigate([APP_ROUTES.cv]);
-        this.toastr.success(`Le cv ${cv.firstname} ${cv.name}`);
-      },
-      error: (err) => {
-        this.toastr.error(
-          `Une erreur s'est produite, Veuillez contacter l'admin`
-        );
-      },
-    });
-  }
+  addCv() {}
 
   get name(): AbstractControl {
-    return this.form.get("name")!;
+    return this.form.get('name')!;
   }
   get firstname() {
-    return this.form.get("firstname");
+    return this.form.get('firstname');
   }
   get age(): AbstractControl {
-    return this.form.get("age")!;
+    return this.form.get('age')!;
   }
   get job() {
-    return this.form.get("job");
+    return this.form.get('job');
   }
   get path() {
-    return this.form.get("path");
+    return this.form.get('path');
   }
   get cin(): AbstractControl {
-    return this.form.get("cin")!;
+    return this.form.get('cin')!;
   }
 }
